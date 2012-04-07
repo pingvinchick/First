9bb84de7d95e8782b0858a8f3734ac0c29d566ed
reHceK <rehcek@ubuntu.ubuntu-domain>
2012-04-07 23:32:17 +0300

    init

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :vkontakte, ENV['2893821 '], ENV['rWfdBo7QDmraKZ1eUUJ1']
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vk1::Application.config.secret_token = '291f70d3db20348e16b240956e45043cf1bff54177165db50ed9bf203e5522dadc85e4ec9a91fb2afb2a55e6edbb6fd8f692b57e3c01b1b0f9937942e533e32a'
