21102fe5291de6ab3e29272b5738833dcc9b08d2
reHceK <rehcek@ubuntu.ubuntu-domain>
2012-04-08 00:25:01 +0300

    init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, ENV['2893821 '], ENV['rWfdBo7QDmraKZ1eUUJ1']
+ 
+  provider :vkontakte, '2893821', 'rWfdBo7QDmraKZ1eUUJ1'
+  
 end
